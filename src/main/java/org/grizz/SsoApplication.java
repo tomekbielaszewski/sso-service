cd5196e499188c0ec58f9ecbec6870cbc0e068b7
tomekbielaszewski <tomasz.bielaszewski@intive.com>
2017-03-28 17:50:16 +0200

    Many SSO providers: facebook and github

--- src/main/java/org/grizz/SsoApplication.java
+++ src/main/java/org/grizz/SsoApplication.java
@@ -20,9 +20,12 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
 
 import javax.servlet.Filter;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableOAuth2Client
@@ -64,6 +67,18 @@ public class SsoApplication extends WebSecurityConfigurerAdapter {
   }
 
   @Bean
+  @ConfigurationProperties("github.client")
+  public AuthorizationCodeResourceDetails github() {
+    return new AuthorizationCodeResourceDetails();
+  }
+
+  @Bean
+  @ConfigurationProperties("github.resource")
+  public ResourceServerProperties githubResource() {
+    return new ResourceServerProperties();
+  }
+
+  @Bean
   public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
     FilterRegistrationBean registration = new FilterRegistrationBean();
     registration.setFilter(filter);
@@ -72,13 +87,29 @@ public class SsoApplication extends WebSecurityConfigurerAdapter {
   }
 
   private Filter ssoFilter() {
+
+    CompositeFilter filter = new CompositeFilter();
+    List<Filter> filters = new ArrayList<>();
+
     OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
     OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
     facebookFilter.setRestTemplate(facebookTemplate);
     UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
     tokenServices.setRestTemplate(facebookTemplate);
     facebookFilter.setTokenServices(tokenServices);
-    return facebookFilter;
+    filters.add(facebookFilter);
+
+    OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+    OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
+    githubFilter.setRestTemplate(githubTemplate);
+    tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
+    tokenServices.setRestTemplate(githubTemplate);
+    githubFilter.setTokenServices(tokenServices);
+    filters.add(githubFilter);
+
+    filter.setFilters(filters);
+    return filter;
+
   }
 
   public static void main(String[] args) {
