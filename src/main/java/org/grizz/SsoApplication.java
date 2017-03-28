f8311cdc3e5bd3b6c9cf8267f24a57cd4da067f0
tomekbielaszewski <tomasz.bielaszewski@intive.com>
2017-03-28 16:27:38 +0200

    logout button with CSRF protection enabled

--- src/main/java/org/grizz/SsoApplication.java
+++ src/main/java/org/grizz/SsoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,9 @@ public class SsoApplication extends WebSecurityConfigurerAdapter {
 			.antMatchers("/", "/login**", "/webjars/**")
 			.permitAll()
 			.anyRequest()
-			.authenticated();
+			.authenticated()
+      .and().logout().logoutSuccessUrl("/").permitAll()
+      .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
 	public static void main(String[] args) {
