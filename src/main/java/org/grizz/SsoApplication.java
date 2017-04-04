5e1edcf0613593b69fae0ef4135f8e9f8c7844f4
Grizwold <tomekbielaszewski@gmail.com>
2017-04-04 09:12:38 +0200

    failed attempt to use AuthSuccessHandler

--- src/main/java/org/grizz/SsoApplication.java
+++ src/main/java/org/grizz/SsoApplication.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticat
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import org.springframework.web.filter.CompositeFilter;
 import javax.servlet.Filter;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -32,87 +34,95 @@ import java.util.List;
 @RestController
 public class SsoApplication extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private OAuth2ClientContext oauth2ClientContext;
+    @Autowired
+    private OAuth2ClientContext oauth2ClientContext;
 
-  @RequestMapping("/user")
-  public Principal user(Principal principal) {
-    return principal;
-  }
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .antMatcher("/**")
-      .authorizeRequests()
-      .antMatchers("/", "/login**", "/webjars/**")
-      .permitAll()
-      .anyRequest()
-      .authenticated()
-      .and().logout().logoutSuccessUrl("/").permitAll()
-      .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-      .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
 
-  @Bean
-  @ConfigurationProperties("facebook.client")
-  public AuthorizationCodeResourceDetails facebook() {
-    return new AuthorizationCodeResourceDetails();
-  }
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
 
-  @Bean
-  @ConfigurationProperties("facebook.resource")
-  public ResourceServerProperties facebookResource() {
-    return new ResourceServerProperties();
-  }
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
 
-  @Bean
-  @ConfigurationProperties("github.client")
-  public AuthorizationCodeResourceDetails github() {
-    return new AuthorizationCodeResourceDetails();
-  }
+    @Bean
+    @ConfigurationProperties("github.client")
+    public AuthorizationCodeResourceDetails github() {
+        return new AuthorizationCodeResourceDetails();
+    }
 
-  @Bean
-  @ConfigurationProperties("github.resource")
-  public ResourceServerProperties githubResource() {
-    return new ResourceServerProperties();
-  }
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties githubResource() {
+        return new ResourceServerProperties();
+    }
 
-  @Bean
-  public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-    FilterRegistrationBean registration = new FilterRegistrationBean();
-    registration.setFilter(filter);
-    registration.setOrder(-100);
-    return registration;
-  }
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
 
-  private Filter ssoFilter() {
+    private Filter ssoFilter() {
 
-    CompositeFilter filter = new CompositeFilter();
-    List<Filter> filters = new ArrayList<>();
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
 
-    OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
-    OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
-    facebookFilter.setRestTemplate(facebookTemplate);
-    UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
-    tokenServices.setRestTemplate(facebookTemplate);
-    facebookFilter.setTokenServices(tokenServices);
-    filters.add(facebookFilter);
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+        tokenServices.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(tokenServices);
+//    facebookFilter.setAuthenticationSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+//      LinkedHashMap details = (LinkedHashMap)((OAuth2Authentication) authentication).getUserAuthentication().getDetails();
+//      System.out.println(details);
+//    });
+        filters.add(facebookFilter);
 
-    OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
-    OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
-    githubFilter.setRestTemplate(githubTemplate);
-    tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
-    tokenServices.setRestTemplate(githubTemplate);
-    githubFilter.setTokenServices(tokenServices);
-    filters.add(githubFilter);
+        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
+        githubFilter.setRestTemplate(githubTemplate);
+        tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
+        tokenServices.setRestTemplate(githubTemplate);
+        githubFilter.setTokenServices(tokenServices);
+        githubFilter.setAuthenticationSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+            LinkedHashMap details = (LinkedHashMap) ((OAuth2Authentication) authentication).getUserAuthentication().getDetails();
+            System.out.println(details);
+        });
+        filters.add(githubFilter);
 
-    filter.setFilters(filters);
-    return filter;
+        filter.setFilters(filters);
+        return filter;
+    }
 
-  }
 
-  public static void main(String[] args) {
-    SpringApplication.run(SsoApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
 }
